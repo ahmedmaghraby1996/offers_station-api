0c4e03c3f8ec4576f7251b9e345b2dc7d5617ed8
Ahmed <a.maghraby96@yahoo.com>
2025-07-13 12:15:00 +0300

    chat

--- src/modules/chat/chat.controller.ts
+++ src/modules/chat/chat.controller.ts
@@ -1,20 +1,22 @@
-// chat.controller.ts
 import { Controller, Post, Body, Get, Param } from '@nestjs/common';
 import { ChatService } from './chat.service';
+import { ApiTags, ApiParam } from '@nestjs/swagger';
 import { ActionResponse } from 'src/core/base/responses/action.response';
 
+@ApiTags('Chat')
 @Controller('chat')
 export class ChatController {
   constructor(private chatService: ChatService) {}
 
-
-  
   @Post('start')
+  @ApiParam({ name: 'client_id', required: true, type: String })
+  @ApiParam({ name: 'store_id', required: true, type: String })
   async startChat(@Body() body: { client_id: string; store_id: string }) {
-    return new ActionResponse(await this.chatService.startChat(body.client_id, body.store_id));
+    return new ActionResponse( await this.chatService.startChat(body.client_id, body.store_id));
   }
 
   @Post(':chat_id/send')
+  @ApiParam({ name: 'chat_id', required: true, type: String, description: 'ID of the chat' })
   async sendMessage(
     @Param('chat_id') chat_id: string,
     @Body() body: { sender_id: string; content: string },
@@ -23,17 +25,16 @@ export class ChatController {
   }
 
   @Get(':chat_id/messages')
+  @ApiParam({ name: 'chat_id', required: true, type: String, description: 'ID of the chat' })
   async getMessages(@Param('chat_id') chat_id: string) {
     return new ActionResponse(await this.chatService.getMessages(chat_id));
   }
 
-  // chat.controller.ts (continued)
+  @Get('all')
 
-@Get('user/:userId/:role')
-async getChats(
- 
-) {
-  return new ActionResponse( await this.chatService.getUserChats());
-}
-
+  async getChats(
+    
+  ) {
+    return new ActionResponse(await this.chatService.getUserChats());
+  }
 }
