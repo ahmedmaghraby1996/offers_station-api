c09b1e193616c6408ef96bff95c01c06ff8d05ee
Ahmed <a.maghraby96@yahoo.com>
2025-07-21 10:52:40 +0300

    offer details

--- src/modules/offers/offers.controller.ts
+++ src/modules/offers/offers.controller.ts
@@ -186,7 +186,7 @@ export class OffersController {
     const result = plainToInstance(OfferResponse, offers, {
       excludeExtraneousValues: true,
     });
-  const response = this._i18nResponse.entity(result);
+    const response = this._i18nResponse.entity(result);
     return new ActionResponse(response);
   }
   //DELETE OFFER
@@ -237,7 +237,7 @@ export class OffersController {
 
     const total = await this.offersService.count(query);
     const offers = await this.offersService.findAll(query);
-  
+
     const result = plainToInstance(OfferResponse, offers, {
       excludeExtraneousValues: true,
     });
@@ -246,5 +246,25 @@ export class OffersController {
     return new PaginatedResponse(response, {
       meta: { total, ...query },
     });
+  }
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
+  @Roles(Role.STORE, Role.CLIENT)
+  @Get('details/:id')
+  async getOfferById(@Param('id') id: string) {
+    const offer = await this.offersService.findOne(id);
+    if (!offer) {
+      throw new NotFoundException('Offer not found');
+    }
+    const result = plainToInstance(OfferResponse, offer, {
+      excludeExtraneousValues: true,
+    });
+    result.is_favorite =
+      offer.favorites?.some(
+        (favorite) => String(favorite.user_id) === String(this.request.user.id),
+      ) ?? false;
+    const response = this._i18nResponse.entity(result);
+
+    return new ActionResponse(response);
   }
 }
