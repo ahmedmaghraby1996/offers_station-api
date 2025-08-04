5f74cca9f46d7df9a084c7dfcee26697722ece5c
Ahmed <a.maghraby96@yahoo.com>
2025-08-04 10:29:50 +0300

    store active

--- src/infrastructure/entities/user/user.entity.ts
+++ src/infrastructure/entities/user/user.entity.ts
@@ -1,3 +1,4 @@
+import { Expose } from 'class-transformer';
 import { AuditableEntity } from 'src/infrastructure/base/auditable.entity';
 import {
   Entity,
@@ -26,134 +27,151 @@ import { City } from '../city/city.entity';
 import { Chat } from '../chat/chat.entity';
 import { Store } from '../store/store.entity';
 
-
 @Entity()
 export class User extends AuditableEntity {
-  // account > unique id generator 10 numbers)
   @Factory((faker) => faker.phone.number('########'))
   @Column({ length: 8, unique: true })
+  @Expose()
   account: string;
-@OneToMany(()=>Store, (store) => store.user)
+
+  @OneToMany(() => Store, (store) => store.user)
+  @Expose()
   stores: Store[];
-  @ManyToOne(()=>City,)
-  city:City
-  @Column({nullable:true})
-  city_id:string
 
-  @OneToMany(()=>Chat, (chat) => chat.client)
-  client_chats: Chat[]
+  @ManyToOne(() => City)
+  @Expose()
+  city: City;
 
-  @OneToMany(()=>Chat, (chat) => chat.store)
-  store_chats: Chat[]
+  @Column({ nullable: true })
+  @Expose()
+  city_id: string;
+
+  @OneToMany(() => Chat, (chat) => chat.client)
+  @Expose()
+  client_chats: Chat[];
+
+  @OneToMany(() => Chat, (chat) => chat.store)
+  @Expose()
+  store_chats: Chat[];
 
   @OneToMany(() => NotificationEntity, (notification) => notification.user)
+  @Expose()
   notifications: NotificationEntity[];
 
   @Factory((faker) => faker.helpers.unique(faker.internet.domainName))
   @Column({ length: 100, unique: true })
+  @Expose()
   username: string;
 
   @Column({ length: 100 })
+  @Expose()
   name: string;
 
-  @OneToMany(
-    () => SuggestionsComplaints,
-    (suggestionsComplaints) => suggestionsComplaints.user,
-  )
+  @OneToMany(() => SuggestionsComplaints, (s) => s.user)
+  @Expose()
   suggestionsComplaints: SuggestionsComplaints[];
 
-
-
-  // @Factory((faker, ctx) => faker.internet.password())
   @Column({ nullable: true, length: 60 })
+  @Expose()
   password: string;
 
   @Factory((faker, ctx) => faker.internet.email(ctx.name))
   @Column({ nullable: true, length: 100 })
+  @Expose()
   email: string;
 
   @Factory((faker) => faker.date.future())
   @Column({ nullable: true })
+  @Expose()
   email_verified_at: Date;
 
   @Factory((faker) => faker.phone.number('+965#########'))
   @Column({ nullable: true, length: 20 })
+  @Expose()
   phone: string;
 
   @Factory((faker) => faker.date.future())
   @Column({ nullable: true })
+  @Expose()
   phone_verified_at: Date;
 
   @Factory((faker) => faker.internet.avatar())
   @Column({ nullable: true, length: 500 })
+  @Expose()
   avatar: string;
 
   @Factory((faker) => faker.helpers.arrayElement(Object.values(Gender)))
   @Column({ nullable: true, type: 'enum', enum: Gender })
+  @Expose()
   gender: Gender;
 
-
-  // @OneToMany(() => WatchUser, (watchUser) => watchUser.parent)
-  // clients: WatchUser[];
-
-
-
   @Column({ nullable: true, length: 500 })
+  @Expose()
   fcm_token: string;
 
   @Column({ nullable: true })
+  @Expose()
   birth_date: string;
 
   @Column({ type: 'enum', enum: Language, default: Language.EN })
+  @Expose()
   language: Language;
 
   @Column({ default: true })
+  @Expose()
   is_active: boolean;
 
   @OneToOne(() => Wallet, (wallet) => wallet.user)
+  @Expose()
   wallet: Wallet;
 
   @ManyToOne(() => Transaction, (transaction) => transaction.user)
+  @Expose()
   transactions: Transaction[];
 
-
   @Column({ type: 'set', enum: Role, default: [Role.CLIENT] })
+  @Expose()
   roles: Role[];
 
   @OneToMany(() => Address, (address) => address.user, {
     cascade: true,
   })
+  @Expose()
   addresses: Promise<Address[]>;
 
   @OneToMany(() => User, (user) => user.user)
+  @Expose()
   family: User[];
 
   @ManyToOne(() => User, (user) => user.family)
   @JoinColumn({ name: 'user_id' })
+  @Expose()
   user: User;
 
   @Column({ nullable: true })
+  @Expose()
   user_id: string;
 
   @Column({ nullable: true })
+  @Expose()
   relation_type: string;
 
-  @ManyToMany(()=>User,)
+  @ManyToMany(() => User)
   @JoinTable()
-  school_users: User[]
+  @Expose()
+  school_users: User[];
+
   constructor(partial: Partial<User>) {
     super();
     Object.assign(this, partial);
   }
 
-  // generate unique id in this pattern: ######
   public uniqueIdGenerator(): string {
     return randNum(8);
   }
 
   @BeforeInsert()
   generateAccount() {
-    // ensure the account is unique
     if (!this.account) this.account = this.uniqueIdGenerator();
   }
 }
