4be532d7bdd5dffc92585abe7e3f44d8a3e23c26
Ahmed <a.maghraby96@yahoo.com>
2025-08-25 10:58:22 +0300

    test payment

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -261,7 +261,11 @@ export class UserService extends BaseService<User> {
       currency,
       amount,
       requestHash,
-      udf1:package_id
+      udf1:package_id,
+      usdf2: 'udf2',
+      udf3: 'udf3',
+      udf4: 'udf4',
+      udf5: 'udf5',
     };
 
     try {
