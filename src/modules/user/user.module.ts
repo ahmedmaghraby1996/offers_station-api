ab206644c9dbed76ea2ef6493de6a21da743b7e3
Ahmed <a.maghraby96@yahoo.com>
2025-10-29 07:16:23 +0300

    add wallet

--- src/modules/user/user.module.ts
+++ src/modules/user/user.module.ts
@@ -3,10 +3,12 @@ import { Module } from '@nestjs/common';
 import { Global } from '@nestjs/common/decorators';
 import { UserController } from './user.controller';
 import { TransactionService } from '../transaction/transaction.service';
+import { TransactionModule } from '../transaction/transaction.module';
+import { NotificationModule } from '../notification/notification.module';
 
 @Global()
 @Module({
-    imports: [],
+    imports: [TransactionModule,NotificationModule],
     controllers: [UserController],
     providers: [UserService,TransactionService],
     exports: [UserService]
